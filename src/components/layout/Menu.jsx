2d54e0e89e2253b76924ce5950614c6e078f5635
Roger Franco <rogerfrancogalt@gmail.com>
2021-11-08 22:40:51 -0400

    Criação dos componentes e arquivos

---
+++ src/components/layout/Menu.jsx
@@ -0,0 +1,12 @@
+import './Menu.css'
+import React from 'react';
+
+const Menu = props => {
+  return (
+    <aside className="Menu">
+      Component Menu
+    </aside>
+    )
+};
+
+export default Menu;
---
+++ src/views/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import React from 'react';
+
+const App = props => {
+  return (
+    <div className="App">
+      Component App
+    </div>
+    )
+};
+
+export default App;
