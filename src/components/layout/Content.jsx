da20ff27f1fa9359e4425f1b3cf4964fd3a9780b
Roger Franco <rogerfrancogalt@gmail.com>
2021-11-11 17:07:59 -0400

    Criando a rota NotFound

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -5,14 +5,16 @@ import {Switch, Route} from 'react-router-dom'
 import About from "../../views/examples/About";
 import Home from "../../views/examples/Home";
 import Param from "../../views/examples/Param";
+import NotFound from "../../views/examples/NotFound";
 
 const Content = (props) => {
   return (
       <main className="Content">
         <Switch>
-        <Route path="/about" exact component={About}/>
-        <Route path="/param/:id" exact component={Param}/>
+        <Route path="/about"  component={About}/>
+        <Route path="/param/:id"  component={Param}/>
         <Route path="/" exact component={Home}/>
+        <Route path="*" component={NotFound}/>
         </Switch>
       </main>
   );
