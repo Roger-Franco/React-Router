8fa564abfeb0cc26a9dfb6dca234375d30e39a20
Roger Franco <rogerfrancogalt@gmail.com>
2021-11-10 22:48:05 -0400

    Usando o método Params

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -4,12 +4,14 @@ import {Switch, Route} from 'react-router-dom'
 
 import About from "../../views/examples/About";
 import Home from "../../views/examples/Home";
+import Param from "../../views/examples/Param";
 
 const Content = (props) => {
   return (
       <main className="Content">
         <Switch>
         <Route path="/about" exact component={About}/>
+        <Route path="/param/:id" exact component={Param}/>
         <Route path="/" exact component={Home}/>
         </Switch>
       </main>
